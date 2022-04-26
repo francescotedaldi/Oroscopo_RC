e72b3a76bf6e173f70055ae26d2a8175b6ae4ce1
francescotedaldi <francescoteda@gmail.com>
2022-04-26 18:14:37 +0200

    esercizio docker compose

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
-const express = require('express');
-const path = require('path');
-const winston = require('winston');
+// https://expressjs.com/
+// npm install express
 
-const app = express();
+// express fa mashup: prende info in giro
+// così ho invocato il metodo che fa il protocollo per me
+var express = require('express');
+var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Test con POSTMAN o CURL
+// CURL https://curl.haxx.se/
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// curl localhost:8888
 
-app.get('/', (req, res) => {
-  const environment = {
-    title: 'Docker with Nginx and Express',
-    node: process.env.NODE_ENV,
-    instance: process.env.INSTANCE,
-    port: process.env.PORT
-  };
-
-  res.render('index', { environment });
+app.get('/', function (req, res) {
+  res.send('Sono il root!!!');
 });
 
-app.listen(process.env.PORT, () => {
-  winston.info(`NODE_ENV: ${process.env.NODE_ENV}`);
-  winston.info(`INSTANCE: ${process.env.INSTANCE}`);
-  winston.info(`EXPRESS: ${process.env.PORT}`);
+// curl -X GET http://localhost:8888/prima_risorsa_get
+
+app.get('/prima_risorsa_get', function (req, res) {
+  res.send('sono la prima risorsa GET su questo server');
+});
+
+app.get('/stessa_risorsa', function (req, res) {
+  res.send('sono la stessa_risorsa acceduta in GET');
+});
+
+app.get('/stato_server', function (req, res) {
+  res.send('nodo: ' + process.env.NODE_ENV + '  istanza: ' + process.env.INSTANCE + '  porta: ' + process.env.PORT);
+});
+
+app.post('/stessa_risorsa', function (req, res) {
+  res.send('sono la stessa_risorsa acceduta in POST');
+});
+
+app.get('/seconda_risorsa_get', function (req, res) {
+  res.send('sono la seconda risorsa GET su questo server');
+});
+
+// curl -X POST http://localhost:8888/prima_risorsa_post
+app.post('/prima_risorsa_post', function (req, res) {
+  res.send('sono la prima risorsa POST su questo server');
+});
+
+
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
