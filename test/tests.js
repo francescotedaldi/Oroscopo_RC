fe4a84cd9c1da537ef0563324409efca7dfb173a
GiovanniMilletti <milletti.1884164@studenti.uniroma1.it>
2022-06-18 16:53:39 +0200

    aggiunti alcuni test e altre github actions
    aggiunto tests.js per i test con mocha
    aggiunte alcune github actions in github-action.yml

---
+++ test/tests.js
@@ -0,0 +1,31 @@
+var should = require('chai').should();
+var assert = require('chai').assert
+var https = require('https');
+const axios = require('axios');
+
+const agent = new https.Agent({  
+    rejectUnauthorized: false
+  });
+
+  describe("Test nginx is proxing in http", function() {
+    it("should return 200", function(done) {
+        axios.get('http://localhost:80/', {httpsAgent: agent}).then(function(response) {
+	       response.status.should.equal(200);
+		   done();
+	}).catch(function(error) {
+		   done(error);
+	});
+  });
+});
+
+describe("Test nginx is proxing in https", function() {
+    it("should return 200", function(done) {
+        axios.get('https://localhost:443/', {httpsAgent: agent}).then(function(response) {
+	       response.status.should.equal(200);
+		   done();
+	}).catch(function(error) {
+		   done(error);
+	});
+  });
+});
+
